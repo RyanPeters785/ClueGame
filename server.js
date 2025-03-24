5a2154263a7016b291d573fd0449a3b54af67ea7
RyanPeters785 <peter785@purdue.edu>
2025-03-23 23:44:05 -0400

    fixing load in

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/api/players', gameController.getPlayerCount);
 app.post('/api/start-game', gameController.startGame);
 app.get('/game', gameController.gamePage);
 app.get('/deduction', gameController.deductionPage);  // New route for deduction page
+app.get('/waiting', (req, res) => {
+    if (req.session.playerName && players.includes(req.session.playerName)) {
+        res.render('waiting', { playerName: req.session.playerName });
+    } else {
+        res.redirect('/login');
+    }
+});
 
 // Start the server
 app.listen(port, () => {
